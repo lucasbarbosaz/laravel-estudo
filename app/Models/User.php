6778bce15f438fd45bc89cc27fa5f84bc33f50df
Lucas Barbosa <corplaxus@gmail.com>
2025-01-13 21:57:11 -0300

    middlewares and middleware route specif

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,8 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function isAdmin(): bool {
+        return in_array($this->email, config('custom.admins'));
+    }
 }
---
+++ config/custom.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'admins' => [
+        'corplaxus@gmail.com'
+    ]
+];
+
+?>
